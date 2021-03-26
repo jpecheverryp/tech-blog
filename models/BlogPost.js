25f9dca05bd5d967e41b9a407552327c32ed16e7
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-03-26 01:14:21 -0400

    created model for blog posts

---
+++ models/BlogPost.js
@@ -0,0 +1,41 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newPost) => {
+                newPost.user_id = req.session.user_id;
+                return newPost
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogpost'
+    }
+);
+
+module.exports = BlogPost;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User');
+const BlogPost = require('./BlogPost');
 
-module.exports = { User };
+module.exports = { User, BlogPost };
