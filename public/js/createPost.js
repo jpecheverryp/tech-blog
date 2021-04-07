3576f24dd07c8caab02fa461190cff0b491a6d4c
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-04-07 03:00:32 -0400

    created logic to show form when button is clicked

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -1,5 +1,20 @@
 const createBtn = document.getElementById('create-btn');
+const postForm = document.getElementById('create-form');
+let actualView = 'posts'
 
-createBtn.addEventListener('click', () => {
-    console.log('hey');
-})
+createBtn.addEventListener('click', switchView)
+
+function switchView() {
+    if (actualView === 'posts') {
+        postForm.style.display = 'block';
+        createBtn.textContent = 'View Posts'
+        actualView = 'createNew';
+        return
+    }
+    if (actualView === 'createNew') {
+        postForm.style.display = 'none';
+        createBtn.textContent = 'New Post'
+        actualView = 'posts';
+        return
+    }
+}
