543c89d7883677e86b77e82b0476cef44678f7b6
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-04-12 01:40:10 -0400

    created handlebars page for single post

--- controllers/blogpostRoutes.js
+++ controllers/blogpostRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { User, BlogPost } = require('../models');
 const withAuth = require("../utils/auth");
+const blogPostParser = require("../utils/blogPostParser")
 
 router.get('/:id', withAuth, async (req, res) => {
     try {
-        const blogpostsData = await BlogPost.findByPk(req.params.id, {
+        let blogpostData = await BlogPost.findByPk(req.params.id, {
             include: [
                 {
                     model: User,
@@ -14,10 +15,16 @@ router.get('/:id', withAuth, async (req, res) => {
                 }],
 
         });
-        if (blogpostsData === null) {
-            res.status(404).send('Post not found')
-        }
-        res.status(200).json(blogpostsData)
+        if (blogpostData) {
+            blogpostData = [blogpostData]
+            const blogposts = blogPostParser(blogpostData);
+            console.log('IM HERE ------========---------');
+            console.log(blogposts);
+            res.render("singlepost", {
+              logged_in: req.session.logged_in,
+              blogposts
+            });
+          };
     } catch (error) {
         res.status(500).json(error)
     }
